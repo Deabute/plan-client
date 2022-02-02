e4e2e8d9eda633cc97f598f517f174da2885e080
Paul Beaudet <inof8or@gmail.com>
2022-02-02 16:34:37 -0500

    Make a record action button component

---
+++ src/components/ActionButtons/RecordActionButton.svelte
@@ -0,0 +1,18 @@
+<!-- RecordActionButton Copyright 2022 Paul Beaudet MIT License -->
+<script lang="ts">
+  import RecordBtn from 'svelte-bootstrap-icons/lib/RecordBtn';
+  import { newTimeStamp, refreshTime } from '../../stores/timeStore';
+
+  export let id: string;
+  export let body: string;
+  export let size = '1';
+
+  const record = async () => {
+    await newTimeStamp(id, body);
+    await refreshTime(false);
+  };
+</script>
+
+<div class={`col-${size} text-danger`} type="button" on:click={record}>
+  <RecordBtn />
+</div>
